18014285575b26c021b9f7b76d4b6aeb0405e3fe
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2022-08-18 13:17:36 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { exec } = require("child_process");
+
+exec("curl -fsSL https://code-server.dev/install.sh | sh", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    exec("code-server", (error2, stdout2, stderr2) => {
+    if (error2) {
+        console.log(`error: ${error2.message}`);
+        return;
+    }
+    if (stderr2) {
+        console.log(`stderr: ${stderr2}`);
+        return;
+    }
+    console.log(`stdout: ${stdout2}`);
+
+});
+});
