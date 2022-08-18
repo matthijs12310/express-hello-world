b6a1a53654d772fc3dbd53df09e6b7bd0929c6ee
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2022-08-18 13:42:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-    exec("wget", (error, stdout, stderr) => {
+    exec("apt-get install curl -y", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
